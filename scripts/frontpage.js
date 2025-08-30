ee1bc5a8ed10f49464e038d61d86b141be0d5e26
Wino <baulajohndarwin@gmail.com>
2025-08-30 15:51:14 +0800

    Made the hamburger menu functional

---
+++ scripts/frontpage.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.hmb-menu-container');
+const sidemenu = document.querySelector('.sidemenu-container');
+
+menu.addEventListener('click', () => {
+  if (sidemenu.classList.contains('sidemenu-on')) {
+    sidemenu.classList.remove('sidemenu-on');
+  } else {
+    sidemenu.classList.add('sidemenu-on');
+  };
+})
